f193c3ec66fed264b907655e710cabc0c5c6b026
DayColor <2005alonlivne@gmail.com>
2020-04-23 16:42:05 +0300

    feat(Client): guilds

--- src/structures/Bot.ts
+++ src/structures/Bot.ts
@@ -1,10 +1,15 @@
+import Guild from './Guild';
+import Cluster from '../Cluster';
 import BotDispatchHandlers from '../socket/BotDispatchHandlers';
 import BotSocket from '../socket/BotSocket';
+import { Snowflake } from '../types';
 
 class Bot {
   private readonly token: string;
   private readonly socket: BotSocket;
   private readonly dispatchHandlers: BotDispatchHandlers;
+
+  public guilds: Cluster<Snowflake, Guild>;
 
   constructor(token: string) {
     this.token = token;
@@ -12,6 +17,8 @@ class Bot {
     this.socket = new BotSocket(this, token);
 
     this.dispatchHandlers = new BotDispatchHandlers();
+
+    this.guilds = new Cluster<Snowflake, Guild>();
   }
 
   public async connect(): Promise<void> {
