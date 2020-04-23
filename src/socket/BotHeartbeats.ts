e5ac393bf0cd96646f3e2d275d9c66c0fe053bc9
DayColor <2005alonlivne@gmail.com>
2020-04-23 16:39:41 +0300

    feat(Heartbeats): ack

--- src/socket/BotHeartbeats.ts
+++ src/socket/BotHeartbeats.ts
@@ -1,24 +1,70 @@
 import WebSocket from 'ws';
-import BotSocket, { OPCodes } from './BotSocket';
+import BotSocket from './BotSocket';
+import { OPCodes } from './constants';
+
+interface HeartbeatData {
+  op: OPCodes.Heartbeat;
+  d: number;
+}
+
+interface HeartbeatInterval {
+  timeout: number;
+  executor: NodeJS.Timeout;
+}
 
 class BotHeartbeats {
-  private ws: WebSocket;
+  private botSocket: BotSocket;
+  private readonly ws: WebSocket;
   private readonly sequence: number;
-  public interval: number;
+  private acked: boolean;
+  public interval: HeartbeatInterval;
 
   constructor(botSocket: BotSocket) {
+    this.botSocket = botSocket;
     this.ws = botSocket.ws;
+
     this.sequence = botSocket.sequence;
 
-    this.interval = 0;
+    this.acked = true;
+
+    this.interval = {
+      timeout: 0,
+      executor: null,
+    };
   }
 
   public start(): void {
-    setInterval(() => {
-      this.ws.send(BotSocket.pack({ op: OPCodes.Heartbeat, d: this.sequence }), (err) => {
-        if (err) console.error(err);
-      });
-    }, this.interval);
+    this.interval.executor = setInterval(this.sendHeartbeat.bind(this), this.interval.timeout);
+  }
+
+  public sendHeartbeat(): void {
+    if (!this.acked) {
+      this.ackFailed();
+      return;
+    }
+
+    this.acked = false;
+    this.ws.send(BotSocket.pack(this.heartbeatData), (err) => {
+      if (err) throw err;
+    });
+  }
+
+  public stopHeartbeat(): void {
+    this.interval.timeout = -1;
+    clearInterval(this.interval.executor);
+  }
+
+  private ackFailed(): void {
+    this.botSocket.close();
+    this.botSocket.connect(true);
+  }
+
+  private get heartbeatData(): HeartbeatData {
+    return { op: OPCodes.Heartbeat, d: this.sequence };
+  }
+
+  public receivedAck(): void {
+    this.acked = true;
   }
 }
 
