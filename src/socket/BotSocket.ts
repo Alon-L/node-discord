2ca5f9ed8d2f866e213a69eb273e31f2a455907a
DayColor <2005alonlivne@gmail.com>
2020-04-23 16:41:01 +0300

    feat(Socket): close and resume

--- src/socket/BotSocket.ts
+++ src/socket/BotSocket.ts
@@ -2,68 +2,23 @@ import fetch from 'node-fetch';
 import WebSocket from 'ws';
 import BotDispatchHandlers from './BotDispatchHandlers';
 import BotHeartbeats from './BotHeartbeats';
+import { OPCodes, GatewayEvents, GatewayCloseCodes, SocketStatus } from './constants';
 import properties, { version, identify } from './properties';
 import Bot from '../structures/Bot';
 
-export enum OPCodes {
-  Dispatch,
-  Heartbeat,
-  Identify,
-  PresenceUpdate,
-  VoiceStateUpdate,
-  Resume = 6,
-  Reconnect,
-  RequestGuildMembers,
-  InvalidSession,
-  Hello,
-  HeartbeatACK,
+export enum BotSocketStatus {
+  Processing,
+  Ready,
+  Closed,
+  Terminated,
 }
 
-export enum GatewayEvents {
-  Ready = 'READY',
-  Resumed = 'RESUMED',
-  Reconnect = 'RECONNECT',
-  InvalidSession = 'INVALID_SESSION',
-  ChannelCreate = 'CHANNEL_CREATE',
-  ChannelUpdate = 'CHANNEL_UPDATE',
-  ChannelDelete = 'CHANNEL_DELETE',
-  ChannelPinsUpdate = 'CHANNEL_PINS_UPDATE',
-  GuildCreate = 'GUILD_CREATE',
-  GuildUpdate = 'GUILD_UPDATE',
-  GuildDelete = 'GUILD_DELETE',
-  GuildBanAdd = 'GUILD_BAN_ADD',
-  GuildBanRemove = 'GUILD_BAN_REMOVE',
-  GuildEmojisUpdate = 'GUILD_EMOJIS_UPDATE',
-  GuildIntegrationsUpdate = 'GUILD_INTEGRATIONS_UPDATE',
-  GuildMemberAdd = 'GUILD_MEMBER_ADD',
-  GuildMemberRemove = 'GUILD_MEMBER_REMOVE',
-  GuildMemberUpdate = 'GUILD_MEMBER_UPDATE',
-  GuildMembersChunk = 'GUILD_MEMBERS_CHUNK',
-  GuildRoleCreate = 'GUILD_ROLE_CREATE',
-  GuildRoleUpdate = 'GUILD_ROLE_UPDATE',
-  GuildRoleDelete = 'GUILD_ROLE_DELETE',
-  InviteCreate = 'INVITE_CREATE',
-  InviteDelete = 'INVITE_DELETE',
-  MessageCreate = 'MESSAGE_CREATE',
-  MessageUpdate = 'MESSAGE_UPDATE',
-  MessageDelete = 'MESSAGE_DELETE',
-  MessageDeleteBulk = 'MESSAGE_DELETE_BULK',
-  MessageReactionAdd = 'MESSAGE_REACTION_ADD',
-  MessageReactionRemove = 'MESSAGE_REACTION_REMOVE',
-  MessageReactionRemoveAll = 'MESSAGE_REACTION_REMOVE_ALL',
-  MessageReactionRemoveEmoji = 'MESSAGE_REACTION_REMOVE_EMOJI',
-  PresenceUpdate = 'PRESENCE_UPDATE',
-  TypingStart = 'TYPING_START',
-  UserUpdate = 'USER_UPDATE',
-  VoiceStateUpdate = 'VOICE_STATE_UPDATE',
-  VoiceServerUpdate = 'VOICE_SERVER_UPDATE',
-  WebhooksUpdate = 'WEBHOOKS_UPDATE',
-}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type PayloadData = any;
 
 export interface Payload {
   op: OPCodes;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  d: any;
+  d: PayloadData;
   s: number;
   t: GatewayEvents;
 }
@@ -71,46 +26,66 @@ export interface Payload {
 class BotSocket {
   private readonly bot: Bot;
   private readonly token: string;
-  private sessionId: string;
   private heartbeats: BotHeartbeats;
+  private status: BotSocketStatus;
   public ws: WebSocket;
+  public sessionId: string;
+
   public sequence: number;
+  private lastSequence: number;
 
   constructor(bot: Bot, token: string) {
     this.bot = bot;
 
     this.token = token;
 
+    this.status = BotSocketStatus.Closed;
+
     this.sequence = null;
+    this.lastSequence = null;
   }
 
-  public async connect(): Promise<void> {
+  public async connect(resume = false): Promise<void> {
     const url = await this.socketURL();
 
     this.ws = new WebSocket(url);
-    this.ws.on('message', this.message.bind(this));
+
+    this.ws.onmessage = this.onMessage.bind(this);
+    this.ws.onclose = this.onClose.bind(this);
 
     this.heartbeats = new BotHeartbeats(this);
+
+    if (resume) {
+      this.ws.onopen = this.resume.bind(this);
+    }
   }
 
-  private async message(data: string): Promise<void> {
+  private async onMessage({ data }): Promise<void> {
     const payload = BotSocket.parse(data);
 
     const { op, d, s } = payload;
+
+    console.log(op);
 
     switch (op) {
       case OPCodes.Dispatch:
         this.sequence = s;
         this.handleDispatch(payload);
         break;
+      case OPCodes.InvalidSession:
+        // Wait 5 seconds and re-identify
+        setTimeout(this.identify.bind(this), 5000);
+        break;
       case OPCodes.Hello:
-        this.heartbeats.interval = d.heartbeat_interval;
+        this.heartbeats.interval.timeout = d.heartbeat_interval;
         this.heartbeats.start();
 
         this.identify();
+
+        this.status = BotSocketStatus.Processing;
         break;
       case OPCodes.HeartbeatACK:
-        // TODO: Heartbeat ACK
+        this.heartbeats.receivedAck();
         break;
     }
   }
@@ -137,8 +112,65 @@ class BotSocket {
 
     // Find the matching event and run it
     if (BotDispatchHandlers.events.has(t)) {
-      BotDispatchHandlers.events.get(t)(this.bot);
+      BotDispatchHandlers.events.get(t)(payload, this.bot, this);
     }
+  }
+
+  public close(code = GatewayCloseCodes.NormalClosure): void {
+    console.log('Terminating connection!');
+
+    // Stop sending heartbeats
+    this.heartbeats.stopHeartbeat();
+
+    if (this.ws) {
+      if (this.ws.readyState !== SocketStatus.Open) {
+        // Remove all socket listeners
+        this.cleanSocket();
+      }
+
+      // Close socket
+      this.ws.close(code);
+    }
+
+    this.ws = null;
+
+    this.lastSequence = this.sequence;
+
+    this.status = BotSocketStatus.Terminated;
+  }
+
+  public ready(): void {
+    this.status = BotSocketStatus.Ready;
+    // TODO: Call the client Ready event
+  }
+
+  private onClose(event: WebSocket.CloseEvent): void {
+    console.error('Close', event.code, event.reason, event.wasClean);
+
+    this.heartbeats.stopHeartbeat();
+
+    if (this.ws) {
+      this.cleanSocket();
+    }
+
+    this.status = BotSocketStatus.Closed;
+  }
+
+  private resume(): void {
+    this.ws.send(
+      BotSocket.pack({
+        op: OPCodes.Resume,
+        d: {
+          token: this.token,
+          session_id: this.sessionId,
+          seq: this.lastSequence || this.sequence,
+        },
+      }),
+    );
+  }
+
+  private cleanSocket(): void {
+    this.ws.onmessage = null;
   }
 
   private static parse(data: string): Payload {
