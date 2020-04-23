171cd07773d5e68c2721736a84d8370f1e494b46
DayColor <2005alonlivne@gmail.com>
2020-04-23 21:05:06 +0300

    docs(JsDoc): botShard

--- src/socket/BotShard.ts
+++ src/socket/BotShard.ts
@@ -3,6 +3,10 @@ import path from 'path';
 import BotShardManager from './BotShardManager';
 import { ShardId } from '../types';
 
+/**
+ * Creates and handles the communication of a shard
+ * @class
+ */
 class BotShard {
   private readonly manager: BotShardManager;
   private process: ChildProcess;
@@ -14,6 +18,10 @@ class BotShard {
     this.id = id;
   }
 
+  /**
+   * Spawns a new child according to the given file path.
+   * Sets the environmental variables for the process with the sharding information
+   */
   public spawn(): void {
     this.process = fork(path.resolve(this.manager.file), [], {
       env: {
