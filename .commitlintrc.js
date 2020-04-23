2ec1e0b574874ed1c592f7b040a00135d93f379d
DayColor <2005alonlivne@gmail.com>
2020-04-23 11:16:42 +0300

    feat(BotSocket): set sessionId

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,4 +2,11 @@ module.exports = {
   extends: [
     '@commitlint/config-conventional',
   ],
+  rules: {
+    'scope-case': [
+      2,
+      'always',
+      'pascal-case',
+    ]
+  }
 };
--- src/socket/BotSocket.ts
+++ src/socket/BotSocket.ts
@@ -17,17 +17,57 @@ export enum OPCodes {
   HeartbeatACK,
 }
 
+export enum GatewayEvents {
+  Ready = 'READY',
+  Resumed = 'RESUMED',
+  Reconnect = 'RECONNECT',
+  InvalidSession = 'INVALID_SESSION',
+  ChannelCreate = 'CHANNEL_CREATE',
+  ChannelUpdate = 'CHANNEL_UPDATE',
+  ChannelDelete = 'CHANNEL_DELETE',
+  ChannelPinsUpdate = 'CHANNEL_PINS_UPDATE',
+  GuildCreate = 'GUILD_CREATE',
+  GuildUpdate = 'GUILD_UPDATE',
+  GuildDelete = 'GUILD_DELETE',
+  GuildBanAdd = 'GUILD_BAN_ADD',
+  GuildBanRemove = 'GUILD_BAN_REMOVE',
+  GuildEmojisUpdate = 'GUILD_EMOJIS_UPDATE',
+  GuildIntegrationsUpdate = 'GUILD_INTEGRATIONS_UPDATE',
+  GuildMemberAdd = 'GUILD_MEMBER_ADD',
+  GuildMemberRemove = 'GUILD_MEMBER_REMOVE',
+  GuildMemberUpdate = 'GUILD_MEMBER_UPDATE',
+  GuildMembersChunk = 'GUILD_MEMBERS_CHUNK',
+  GuildRoleCreate = 'GUILD_ROLE_CREATE',
+  GuildRoleUpdate = 'GUILD_ROLE_UPDATE',
+  GuildRoleDelete = 'GUILD_ROLE_DELETE',
+  InviteCreate = 'INVITE_CREATE',
+  InviteDelete = 'INVITE_DELETE',
+  MessageCreate = 'MESSAGE_CREATE',
+  MessageUpdate = 'MESSAGE_UPDATE',
+  MessageDelete = 'MESSAGE_DELETE',
+  MessageDeleteBulk = 'MESSAGE_DELETE_BULK',
+  MessageReactionAdd = 'MESSAGE_REACTION_ADD',
+  MessageReactionRemove = 'MESSAGE_REACTION_REMOVE',
+  MessageReactionRemoveAll = 'MESSAGE_REACTION_REMOVE_ALL',
+  MessageReactionRemoveEmoji = 'MESSAGE_REACTION_REMOVE_EMOJI',
+  PresenceUpdate = 'PRESENCE_UPDATE',
+  TypingStart = 'TYPING_START',
+  UserUpdate = 'USER_UPDATE',
+  VoiceStateUpdate = 'VOICE_STATE_UPDATE',
+  VoiceServerUpdate = 'VOICE_SERVER_UPDATE',
+  WebhooksUpdate = 'WEBHOOKS_UPDATE',
+}
+
 export interface Payload {
   op: OPCodes;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   d: any;
   s: number;
-  t: string;
+  t: GatewayEvents;
 }
 
 class BotSocket {
   private readonly token: string;
-  // TODO: Set sessionId on READY event
   private sessionId: string;
   private heartbeats: Heartbeats;
   public ws: WebSocket;
@@ -62,15 +102,7 @@ class BotSocket {
         this.heartbeats.interval = d.heartbeat_interval;
         this.heartbeats.start();
 
-        this.ws.send(
-          BotSocket.pack({
-            op: OPCodes.Identify,
-            d: {
-              ...identify,
-              token: this.token,
-            },
-          }),
-        );
+        this.identify();
         break;
       case OPCodes.HeartbeatACK:
         // TODO: Heartbeat ACK
@@ -78,7 +110,24 @@ class BotSocket {
     }
   }
 
+  private identify(): void {
+    this.ws.send(
+      BotSocket.pack({
+        op: OPCodes.Identify,
+        d: {
+          ...identify,
+          token: this.token,
+        },
+      }),
+    );
+  }
+
   private handleDispatch(payload: Payload): void {
+    // Set session ID in case of a Ready event
+    if (payload.t === GatewayEvents.Ready) {
+      this.sessionId = payload.d.session_id;
+    }
+
     console.log(payload.t);
   }
 
