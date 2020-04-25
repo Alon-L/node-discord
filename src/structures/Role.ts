9f1219fbdbc205d8938b25f28e56ef2c59c64f0e
DayColor <2005alonlivne@gmail.com>
2020-04-25 19:22:44 +0300

    feat(Structure): role

---
+++ src/structures/Role.ts
@@ -0,0 +1,65 @@
+import { GatewayStruct } from './BaseStruct';
+import Bot from './Bot';
+import Guild from './Guild';
+import GuildBaseStruct from './GuildBaseStruct';
+import { Snowflake } from '../types';
+
+class Role extends GuildBaseStruct {
+  /**
+   * The role's ID
+   */
+  public id: Snowflake;
+
+  /**
+   * The role name
+   */
+  public name: string;
+
+  /**
+   * Number representation of hexadecimal color code
+   */
+  public color: number;
+
+  /**
+   * Whether this role is listed separately on the guild members list
+   */
+  public listedSeparately: boolean;
+
+  /**
+   * The position of this role
+   */
+  public position: number;
+
+  public permissions: undefined;
+
+  /**
+   * Whether this role is managed by an integration
+   */
+  public managed: boolean;
+
+  /**
+   * Whether this role is mentionable
+   */
+  public mentionable: boolean;
+
+  constructor(bot: Bot, role?: GatewayStruct, guild?: Guild) {
+    super(bot, guild);
+
+    if (role) {
+      this.build(role);
+    }
+  }
+
+  protected build(role: GatewayStruct): void {
+    this.id = role.id;
+    this.name = role.name;
+    this.color = role.color;
+    this.listedSeparately = role.hoist;
+    this.position = role.position;
+    this.permissions = role.permissions;
+    this.managed = role.managed;
+    this.mentionable = role.mentionable;
+  }
+}
+
+export default Role;
