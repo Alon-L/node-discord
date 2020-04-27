9c5d3cfbeea9d7ebbe5ddaebbfe31f10d03ee67b
DayColor <2005alonlivne@gmail.com>
2020-04-27 17:18:18 +0300

    docs(Events): channel delete

--- src/structures/handlers/events/events.ts
+++ src/structures/handlers/events/events.ts
@@ -9,16 +9,25 @@ import Channel from '../../channels/Channel';
  */
 export declare function CHANNEL_CREATE(channel: Channel): void;
 
+// TODO: {@link MESSAGE_CREATE} instead of writing MESSAGE_CREATE
 /**
  * Sent when a channel is updated.
  * This is not sent when the field {@link GuildTextChannel.lastMessageId} is altered.
- * To keep track of the lastMessageId changes, you must listen for MESSAGE_CREATE events.
+ * To keep track of the lastMessageId changes, you must listen for MESSAGE_CREATE events
  * @param {Channel} oldChannel The channel before its modification
  * @param {Channel} newChannel The channel after its modification
  * @asMemberOf BotEvents
  * @event BotEvents#CHANNEL_UPDATE
  */
 export declare function CHANNEL_UPDATE(oldChannel: Channel, newChannel: Channel): void;
+
+/**
+ * Sent when a channel is deleted
+ * @param {Channel} channel The deleted channel
+ * @asMemberOf BotEvents
+ * @event BotEvents#CHANNEL_DELETE
+ */
+export declare function CHANNEL_DELETE(channel: Channel): void;
 
 export declare interface Events {
   on(event: GatewayEvents.ChannelCreate, listener: typeof CHANNEL_CREATE): this;
