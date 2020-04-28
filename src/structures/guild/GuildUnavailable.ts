8b4e6370b273c6b34f2c4f48232537c62db2984e
DayColor <2005alonlivne@gmail.com>
2020-04-28 15:52:50 +0300

    docs(TypeDoc): guildUnavailable class description

--- src/structures/guild/GuildUnavailable.ts
+++ src/structures/guild/GuildUnavailable.ts
@@ -2,6 +2,12 @@ import { Snowflake } from '../../types';
 import BaseStruct, { GatewayStruct } from '../BaseStruct';
 import Bot from '../bot/Bot';
 
+/**
+ * Used instead of {@link Guild} when the guild is unavailable
+ * Includes just the ID of the guild, which should be fetched in order to obtain the full guild class
+ * @class
+ * @extends BaseStruct
+ */
 class GuildUnavailable extends BaseStruct {
   /**
    * Guild ID
