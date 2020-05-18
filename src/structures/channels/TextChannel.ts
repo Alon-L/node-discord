3d764e9282e27fc4f46cd2a0964c2b6bfb2697ac
DayColor <2005alonlivne@gmail.com>
2020-05-18 17:37:42 +0300

    chore(TextChannel): added abstract properties

--- src/structures/channels/TextChannel.ts
+++ src/structures/channels/TextChannel.ts
@@ -1,5 +1,19 @@
 // TODO: Create Channel class and add the type TextBasedChannel = GuildTextChannel | DMChannel with the send.message methods, send.embed, send.files, etc...
 
-abstract class TextChannel {}
+import { Snowflake } from '../../types';
+import Timestamp from '../Timestamp';
+
+abstract class TextChannel {
+  /**
+   * The ID of the last message sent in this channel.
+   * May not point to an existing or valid message
+   */
+  abstract lastMessageId?: Snowflake | null;
+
+  /**
+   * Timestamp of when the last pinned message was pinned
+   */
+  abstract lastPinTimestamp?: Timestamp;
+}
 
 export default TextChannel;
