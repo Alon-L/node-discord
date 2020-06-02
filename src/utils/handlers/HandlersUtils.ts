17d335cc58473a5353aafd9e085efe34df43072a
DayColor <2005alonlivne@gmail.com>
2020-06-02 22:56:58 +0300

    docs(Utils): handlersUtils

--- src/utils/handlers/HandlersUtils.ts
+++ src/utils/handlers/HandlersUtils.ts
@@ -1,8 +1,19 @@
 import { PayloadData } from '../../socket/BotSocketShard';
 import Bot from '../../structures/bot/Bot';
 
+/**
+ * Main class for all util methods for socket event handlers.
+ * @class
+ */
 class HandlersUtils {
+  /**
+   * The bot associated to this socket event
+   */
   protected readonly bot: Bot;
+
+  /**
+   * The data received from the socket event
+   */
   protected readonly data: PayloadData;
 
   constructor(bot: Bot, data: PayloadData) {
