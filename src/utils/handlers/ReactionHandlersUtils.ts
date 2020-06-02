72588db131ed2ffdf42160e1cd357a7485760605
DayColor <2005alonlivne@gmail.com>
2020-06-02 22:57:08 +0300

    docs(Utils): reactionHandlersUtils

--- src/utils/handlers/ReactionHandlersUtils.ts
+++ src/utils/handlers/ReactionHandlersUtils.ts
@@ -3,11 +3,23 @@ import Emoji from '../../structures/Emoji';
 import GuildTextChannel from '../../structures/channels/GuildTextChannel';
 import Message from '../../structures/message/Message';
 
+/**
+ * Provides util methods for all reactions-related handlers
+ * @class
+ */
 class ReactionHandlersUtils extends HandlersUtils {
+  /**
+   * Returns the {@link Emoji} received from the event data
+   * @type {Emoji}
+   */
   public get emoji(): Emoji {
     return new Emoji(this.bot, this.data.emoji);
   }
 
+  /**
+   * Returns the message extracted from the event data
+   * @type {Message | undefined}
+   */
   public get message(): Message | undefined {
     const { guild_id: guildId, channel_id: channelId, message_id: messageId } = this.data;
 
