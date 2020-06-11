a91346599be6c7a56739c812a8e918beea2f12be
DayColor <2005alonlivne@gmail.com>
2020-06-11 22:11:48 +0300

    test: permission flags

---
+++ tests/flags.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+import config from './config.json';
+import { BotEvents } from '../src/socket/constants';
+import { PermissionFlagTypes } from '../src/socket/constants';
+import Bot from '../src/structures/bot/Bot';
+
+const bot = new Bot(config.token);
+bot.connection.connect();
+
+(async function (): Promise<void> {
+  await bot.events.wait(BotEvents.Ready);
+
+  const role = bot.guilds.get('702476896008405002')?.roles.get('706861476752785461');
+  console.log(role?.permissions.has(PermissionFlagTypes.Administrator), 'has permission');
+})();
+
+bot.events.on(BotEvents.Debug, console.log);
