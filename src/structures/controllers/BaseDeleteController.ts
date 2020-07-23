f3b890cf110a832ae7d41be57681a1384ea7d950
Alon-L <2005alonlivne@gmail.com>
2020-07-23 18:55:30 +0300

    feat(Controller): baseDeleteController

---
+++ src/structures/controllers/BaseDeleteController.ts
@@ -0,0 +1,18 @@
+import BaseController from './BaseController';
+import { Snowflake } from '../../types/types';
+import { BaseStructWithId } from '../BaseStruct';
+
+/**
+ * Base controller with delete capabilities
+ * @template T
+ */
+abstract class BaseDeleteController<T extends BaseStructWithId> extends BaseController<T> {
+  /**
+   * Deletes a cached item
+   * @param {Snowflake} id The ID of the item you wish to delete
+   * @returns {Promise<T>}
+   */
+  abstract delete(id: Snowflake): Promise<T | void>;
+}
+
+export default BaseDeleteController;
