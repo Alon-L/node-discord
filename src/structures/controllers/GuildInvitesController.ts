6da3699989ef97ab816d11be42c6994ee62f5c71
Alon-L <2005alonlivne@gmail.com>
2020-07-23 21:17:02 +0300

    feat(Controller): guildInvitesController

---
+++ src/structures/controllers/GuildInvitesController.ts
@@ -0,0 +1,58 @@
+import BaseDeleteController from './base/BaseDeleteController';
+import BaseFetchController from './base/BaseFetchController';
+import { Snowflake } from '../../types/types';
+import Invite from '../Invite';
+import Guild from '../guild/Guild';
+
+/**
+ * Options for when fetching an invite
+ */
+export interface FetchInviteOptions {
+  /**
+   * Whether the invite should contain approximate member counts
+   */
+  withCounts?: boolean;
+}
+
+/**
+ * Provides an interface for a guild's invites cache
+ * The invites are mapped by their invite codes
+ */
+class GuildInvitesController extends BaseFetchController<Invite>
+  implements BaseDeleteController<Invite> {
+  /**
+   * The guild this controller is associated to
+   */
+  public readonly guild: Guild;
+
+  constructor(guild: Guild) {
+    super(guild);
+
+    this.guild = guild;
+  }
+
+  /**
+   * Delete an invite by its invite code
+   * @param {Snowflake} code The invite code
+   * @returns {Promise<Invite>}
+   */
+  public delete(code: Snowflake): Promise<Invite> {
+    return this.bot.api.deleteInvite(code);
+  }
+
+  /**
+   * Fetches an invite by its invite code
+   * @param {string} code The invite code
+   * @param {FetchInviteOptions} options An additional set of options for the invite
+   * @returns {Promise<Invite>}
+   */
+  public async fetch(code: string, options?: FetchInviteOptions): Promise<Invite> {
+    const invite = await this.bot.api.fetchInvite(code, options);
+
+    this.add(invite);
+
+    return invite;
+  }
+}
+
+export default GuildInvitesController;
