316930b8c565402fcf4fe9b49764dc2992026a7e
Alon-L <2005alonlivne@gmail.com>
2020-07-25 23:01:47 +0300

    test(Controller): BotGuildsController

---
+++ tests/controllers/botGuildsController.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+import { BotEvent } from '../../src/socket/constants';
+import Bot from '../../src/structures/bot/Bot';
+import config from '../config.json';
+
+const bot = new Bot(config.token);
+bot.connection.connect();
+
+(async function (): Promise<void> {
+  await bot.events.wait(BotEvent.Ready);
+
+  const guild = await bot.guilds.get('702476896008405002');
+
+  const preview = await bot.guilds.fetchPreview(guild.id);
+
+  console.log(preview);
+})();
