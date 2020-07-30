2785710e428134c24056a196bdca18fbf51eba89
Alon-L <2005alonlivne@gmail.com>
2020-07-30 19:01:15 +0300

    test(GuildEmoji): modify & delete

---
+++ tests/api/guildEmoji.api.ts
@@ -0,0 +1,42 @@
+'use strict';
+
+import { BotEvent } from '../../src/socket/constants';
+import Bot from '../../src/structures/bot/Bot';
+import config from '../config.json';
+
+const bot = new Bot(config.token);
+bot.connection.connect();
+
+(async function (): Promise<void> {
+  await bot.events.wait(BotEvent.Ready);
+
+  const guild = await bot.guilds.fetch('702476896008405002');
+
+  const emoji = await guild.emojis.cache.first;
+
+  if (!emoji) throw new Error('The guild has no emojis!');
+
+  const newName = 'new_emoji_name';
+
+  emoji.modify({ name: 'new_emoji_name' });
+
+  await bot.events.wait(BotEvent.GuildEmojisUpdate);
+
+  console.log(
+    guild.emojis.cache.first?.name === newName,
+    "the updated guild's name is equal to the given one",
+    'expected: true',
+  ); // expected: true
+
+  emoji.delete();
+
+  await bot.events.wait(BotEvent.GuildEmojisUpdate);
+
+  console.log(
+    guild.emojis.cache.firstKey !== emoji.id,
+    'whether the guild emoji was properly deleted',
+    'expected: true',
+  ); // expected: true
+})();
+
+bot.events.on(BotEvent.Debug, console.log);
