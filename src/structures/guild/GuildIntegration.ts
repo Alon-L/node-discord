22390c666f7d5430a4626f62c3841869fc266af6
Alon-L <2005alonlivne@gmail.com>
2020-08-18 20:54:09 +0300

    feat(Struct): guildIntegration

---
+++ src/structures/guild/GuildIntegration.ts
@@ -0,0 +1,140 @@
+import { Guild } from './Guild';
+import { GuildBaseStruct } from './GuildBaseStruct';
+import { Snowflake } from '../../types';
+import { GatewayStruct } from '../BaseStruct';
+import { Role } from '../Role';
+import { Timestamp } from '../Timestamp';
+import { User } from '../User';
+import { Bot } from '../bot';
+
+/**
+ * The behavior of expiring subscribers of an integration
+ */
+export enum IntegrationExpireBehavior {
+  RemoveRole,
+  Kick,
+}
+
+/**
+ * Expire data for an integration
+ */
+export interface IntegrationExpire {
+  /**
+   * The behavior of expiring subscribers
+   */
+  behavior: IntegrationExpireBehavior;
+
+  /**
+   * The grace period (in days) before expiring subscribers
+   */
+  gracePeriod: number;
+}
+
+/**
+ * Account data for an integration
+ */
+export interface IntegrationAccount {
+  /**
+   * The ID of the account
+   */
+  id: string;
+
+  /**
+   * The name of the account
+   */
+  name: string;
+}
+
+/**
+ * Guild integration object
+ */
+export class GuildIntegration extends GuildBaseStruct {
+  /**
+   * The ID of the integration
+   */
+  public id!: Snowflake;
+
+  /**
+   * The name of the integration
+   */
+  public name!: string;
+
+  /**
+   * The type of the integration
+   * @example twitch
+   * @example youtube
+   */
+  public type!: string;
+
+  /**
+   * Whether this integration is enabled
+   */
+  public enabled!: boolean;
+
+  /**
+   * Whether this integration is syncing
+   */
+  public syncing!: boolean;
+
+  /**
+   * The role that this integration uses for "subscribers"
+   */
+  public role!: Role | undefined;
+
+  /**
+   * Whether emoticons should be synced for this integration (twitch only currently)
+   */
+  public enableEmoticons!: boolean;
+
+  /**
+   * The expire data for this integration
+   */
+  public expire!: IntegrationExpire;
+
+  /**
+   * The user for this integration
+   */
+  public user!: User;
+
+  /**
+   * The integration account information
+   */
+  public account!: IntegrationAccount;
+
+  /**
+   * Timestamp of when this integration was last synced
+   */
+  public syncedAt!: Timestamp;
+
+  constructor(bot: Bot, integration: GatewayStruct, guild: Guild) {
+    super(bot, guild, integration);
+
+    this.init(integration);
+  }
+
+  /**
+   * @ignore
+   * @param {GatewayStruct} integration The integration data
+   * @returns {this}
+   */
+  public init(integration: GatewayStruct): this {
+    this.id = integration.id;
+    this.name = integration.name;
+    this.type = integration.type;
+    this.enabled = integration.enabled;
+    this.syncing = integration.syncing;
+
+    this.role = this.guild.roles.cache.get(integration.role_id);
+
+    this.enableEmoticons = integration.enable_emoticons;
+    this.expire = {
+      behavior: integration.expire_behavior,
+      gracePeriod: integration.expire_grace_period,
+    };
+    this.user = this.bot.users.get(integration.user.id) || new User(this.bot, integration.user);
+    this.account = integration.account;
+    this.syncedAt = new Timestamp(integration.synced_at);
+
+    return this;
+  }
+}
