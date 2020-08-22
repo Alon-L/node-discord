a555697dbdde7098b326281ce21c3ddeb6e6cdb0
Alon-L <2005alonlivne@gmail.com>
2020-08-22 14:12:25 +0300

    chore: export structures

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 export * from './api';
 export * from './bot';
 export * from './controllers';
-export * from './socket';
 export * from './sharding';
+export * from './socket';
+export * from './structures';
 export * from './types';
 
 export * as Collection from './Collection';
