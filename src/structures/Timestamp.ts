4a1add025f875761f68e22c671bd42dda3b8b9a0
Alon-L <2005alonlivne@gmail.com>
2020-08-26 13:50:23 +0300

    chore(Timestamp): cleaned up code

--- src/structures/Timestamp.ts
+++ src/structures/Timestamp.ts
@@ -2,12 +2,19 @@
  * Handles the conversion of timestamps received from the Discord API into UNIX timestamps
  */
 export class Timestamp {
-  public date?: string;
+  /**
+   * The ISO date of this timestamp
+   */
+  public date: string | undefined;
 
-  constructor(date: string | undefined | number | Date) {
-    if (typeof date === 'number') this.date = new Date(date).toISOString();
-    else if (date instanceof Date) this.date = date.toISOString();
-    else this.date = date;
+  constructor(date: string | number | Date | undefined) {
+    if (typeof date === 'number') {
+      this.date = new Date(date).toISOString();
+    } else if (date instanceof Date) {
+      this.date = date.toISOString();
+    } else {
+      this.date = date;
+    }
   }
 
   public unix(): number | undefined {
